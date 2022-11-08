33237179784bc86a8bf1f4d6faaf6c4ef1f90f80
AdiFischer <adifischer1@gmail.com>
2022-11-08 11:36:42 -0500

    set auth login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { useState } from 'react'
-import Login from './scenes/login';
+import Login from './scenes/Login';
 import Protected from './scenes/Protected';
 
 function App() {
--- src/scenes/login.jsx
+++ src/scenes/login.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react"
+import { initializeApp } from "firebase/app"
+import { getAuth, signInWithEmailAndPassword} from "firebase/auth"
 
-export default function login() {
+const firebaseConfig = {
+      apiKey: "AIzaSyAXgiYz-K32Zkn1oXjsVjr8fMuUmzEZrck",
+      authDomain: "fir-auth-aff.firebaseapp.com",
+      projectId: "fir-auth-aff",
+      storageBucket: "fir-auth-aff.appspot.com",
+      messagingSenderId: "170909949080",
+      appId: "1:170909949080:web:937032b1080c75b9e3c4ed"
+    };
+
+export default function Login({ setUser }) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault()
+        const app = initializeApp(firebaseConfig)//connects us to Firebase
+        const auth = getAuth(app)//connects us to Firebase Auth
+        const response = 
+            await signInWithEmailAndPassword(auth, email, password)//sign us in
+            .catch(alert);
+        setUser(response.user)
     }
     return (
         <>
@@ -19,17 +36,12 @@ export default function login() {
                 </label><br />
                 <label htmlFor="password">Password:{' '}
                     <input type="password" name="password"
-                    value={password onChange={e => setPassword(e.target.value)}
-
-
-
-                        placeholder="........." />
-
+                    value={password} onChange={e => setPassword(e.target.value)}
+                 placeholder="........." />
                 </label>
                 <br />
                 <button type="submit">Login</button>
             </form>
         </>
-
     )
 }
